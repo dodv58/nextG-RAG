84ea4c4c459bc115fc7a1f3cdad040f0ff0e033c
DoDang <dodv212@gmail.com>
2024-03-08 19:19:40 +0700

    update

--- src/qna.py
+++ src/qna.py
@@ -13,10 +13,7 @@ def qna(model, file, question):
     vectorstore = Chroma(persist_directory=file[2], embedding_function=LlamaCppEmbeddings(model_path=model),)
     # rag_prompt_llama = hub.pull("rlm/rag-prompt-llama")
     # "If you don't know the answer, just say that you don't know. "
-    rag_prompt_llama = PromptTemplate.from_template("[INST]<<SYS>> You are an assistant for question-answering tasks. "
-                                                   "Use the following pieces of retrieved context to answer the question. "
-                                                   "If retrieved context does not contain information to answer the question, just say that you don't know. "
-                                                   "Use three sentences maximum and keep the answer concise.<</SYS>> \nQuestion: {question} \nContext: {context} \nAnswer: [/INST]")
+    rag_prompt_llama = PromptTemplate.from_template('[INST]<<SYS>> You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If the context does not provide the answer to the question, the assistant will say, "I am sorry, but I do not know the answer to that question". The assistant will not invent anything that is not drawn directly from the context and keep the answer concise.<</SYS>> \nQuestion: {question} \nContext: {context} \nAnswer: [/INST]')
     rag_prompt_llama = HumanMessagePromptTemplate.from_template(rag_prompt_llama.template)
     rag_prompt_llama = ChatPromptTemplate.from_messages([rag_prompt_llama])
     llm = LlamaCpp(
