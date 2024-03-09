00ae5f457a856a5dc9d7c3d7b2a364a1681655ab
DoDang <dodv212@gmail.com>
2024-03-09 18:05:21 +0700

    update

--- src/qna.py
+++ src/qna.py
@@ -11,17 +11,17 @@ def format_docs(docs):
 
 def qna(model, file, question):
     vectorstore = Chroma(persist_directory=file[2], embedding_function=LlamaCppEmbeddings(model_path=model),)
-    rag_prompt_llama = hub.pull("rlm/rag-prompt-llama")
 
-#     template = """[INST]<<SYS>> Use the following pieces of context to answer the question at the end. If the provided context does not contain the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum and keep the answer as concise as possible.<</SYS>>
-#
-# Question: {question}
-#
-# Context: {context}
-#
-# Answer: [/INST]
-# """
-#     rag_prompt_llama = PromptTemplate.from_template(template)
+    template = """[INST]<<SYS>> Use the following pieces of context to answer the question at the end. If the provided context does not contain the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum and keep the answer as concise as possible.<</SYS>>
+
+Question: {question}
+
+Context: {context}
+
+Answer: [/INST]
+"""
+    rag_prompt_llama = PromptTemplate.from_template(template)
+    # rag_prompt_llama = hub.pull("rlm/rag-prompt-llama")
 
     llm = LlamaCpp(
         model_path=model,
